f55d6d130e27c7577601f65883a107e7783067dd
OsmaRojasR <osmar.rojas@beyondtech.consulting>
2024-06-25 17:07:15 -0600

    feat: remove unused code in VPN module

--- modules/network/vpn/main.tf
+++ modules/network/vpn/main.tf
@@ -34,7 +34,6 @@ resource "google_compute_vpn_tunnel" "tunnel_0" {
   region                = var.region
   vpn_gateway           = google_compute_ha_vpn_gateway.ha_vpn_gateway.id
   peer_external_gateway = google_compute_external_vpn_gateway.external_gateway.id
-  peer_gcp_gateway      = google_compute_router.router.id
   shared_secret         = var.shared_secret
   router                = google_compute_router.router.name
 }
@@ -44,27 +43,26 @@ resource "google_compute_vpn_tunnel" "tunnel_1" {
   region                = var.region
   vpn_gateway           = google_compute_ha_vpn_gateway.ha_vpn_gateway.id
   peer_external_gateway = google_compute_external_vpn_gateway.external_gateway.id
-  peer_gcp_gateway      = google_compute_router.router.id
   shared_secret         = var.shared_secret
   router                = google_compute_router.router.name
 }
 
 resource "google_compute_router_peer" "bgp_peer_0" {
-  name                  = "${var.vpn_gateway_name}-bgp-peer-0"
-  router                = google_compute_router.router.name
-  region                = var.region
-  peer_asn              = var.vpn_peer_asn
-  peer_ip_address       = var.peer_ip_0
+  name                      = "${var.vpn_gateway_name}-bgp-peer-0"
+  router                    = google_compute_router.router.name
+  region                    = var.region
+  peer_asn                  = var.vpn_peer_asn
+  peer_ip_address           = var.peer_ip_0
   advertised_route_priority = 100
-  interface             = google_compute_vpn_tunnel.tunnel_0.id
+  interface                 = google_compute_vpn_tunnel.tunnel_0.id
 }
 
 resource "google_compute_router_peer" "bgp_peer_1" {
-  name                  = "${var.vpn_gateway_name}-bgp-peer-1"
-  router                = google_compute_router.router.name
-  region                = var.region
-  peer_asn              = var.vpn_peer_asn
-  peer_ip_address       = var.peer_ip_1
+  name                      = "${var.vpn_gateway_name}-bgp-peer-1"
+  router                    = google_compute_router.router.name
+  region                    = var.region
+  peer_asn                  = var.vpn_peer_asn
+  peer_ip_address           = var.peer_ip_1
   advertised_route_priority = 100
-  interface             = google_compute_vpn_tunnel.tunnel_1.id
+  interface                 = google_compute_vpn_tunnel.tunnel_1.id
 }
