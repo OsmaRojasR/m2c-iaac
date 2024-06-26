30d9ced1011429a38aef7f520c0ba1ec76a94292
Jesus Lozano <eduardo.lozano@cascadacon.com>
2024-06-26 17:29:47 -0600

    Modificacion de cloudbuild

--- modules/triggers/cloud_build/main.tf
+++ modules/triggers/cloud_build/main.tf
@@ -2,12 +2,9 @@ resource "google_cloudbuild_trigger" "filename-trigger" {
   name = "m2c-dev-trigger-${var.service_name}"
   location = "us-central1"
 
-  github {
-    owner       = "JesusELozanoR"
-    name        = "m2c-servdocuments"
-    push {
-      branch = "^master$"
-    }
+  trigger_template {
+    branch_name = "^master$"
+    repo_name   = "OsmaRojasR/m2c-ms"
   }
 
   filename = "cloudbuild.yaml"
